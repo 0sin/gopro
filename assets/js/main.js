cee29aa984fceebefdd013e6b8ee3217e6306884
0sin <69961808+0sin@users.noreply.github.com>
2021-02-07 20:20:49 +0900

    Update footer mobile accordion

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,23 +32,63 @@ function findMatchSelect(e, selectName) {
 
 // MOBILE HEADER 
 // HAMBURGER BAR OPEN
+const mobileBarsIcon = document.querySelector('.mobile_gnb_open_icon');
+
+mobileBarsIcon.addEventListener('click', e => {
+  const mobileGnbSub = document.querySelector('.gnb_sub_menu_mobile');
+  const barsIcon = mobileBarsIcon.querySelector('.fa-bars');
+  const closeIcon = mobileBarsIcon.querySelector('.fa-times');
+  mobileGnbSub.classList.toggle('display');
+  barsIcon.classList.toggle('display');
+  closeIcon.classList.toggle('display');
+})
 
 
 // MOBILE GNB SUB OPEN
-const mobileGnbTitleList = document.querySelectorAll('.gnb_menu_mobile_item');
+const mobileGnbListTitle = document.querySelectorAll('.gnb_menu_mobile_item');
 const mobileSubMenuLists = document.querySelectorAll('.gnb_sub_menu_mobile_lists');
-let currnetTargetState = false; //현재 닫힘
+const mobileSubArrowIcons = document.querySelectorAll('.fa-chevron-down');
 
-mobileGnbTitleList.forEach( e => e.addEventListener('click', e => {
-  const currentTargetUl = e.currentTarget.querySelector('.gnb_sub_menu_mobile_lists');
+function toggleMobileAccordion(e) {
+  const currentTargetList = e.currentTarget.querySelector('.gnb_sub_menu_mobile_lists');
   const currentTargetArrowIcon = e.currentTarget.querySelector('.fa-chevron-down');
-  
-  if(currentTargetUl) {
-    // mobileSubMenuLists.forEach(e => e.classList.remove('display'));
-    currentTargetUl.classList.toggle('display');
-    currentTargetArrowIcon.classList
+
+  if (currentTargetList) {
+    if (currentTargetList.classList.contains('display')) {
+      currentTargetList.classList.remove('display');
+      currentTargetArrowIcon.classList.remove('open');
+    } else {
+      mobileSubMenuLists.forEach(e => e.classList.remove('display'));
+      mobileSubArrowIcons.forEach(e => e.classList.remove('open'));
+      currentTargetList.classList.add('display');
+      currentTargetArrowIcon.classList.add('open');
+    }
   } else {
     return;
   } 
-}));
+}
+
+mobileGnbListTitle.forEach( e => e.addEventListener('click', toggleMobileAccordion));
+
+
+
+// FOOTER MOBILE ACCORDION
+const mobilefooterlists = document.querySelectorAll('.footer_list_wrap');
+const mobilefooterIcons = document.querySelectorAll('.footer_m_angle_down');
+
+function openFooterList(e) {
+  const currentFooterLists = e.currentTarget.classList;
+  const currentFooterIcons = e.currentTarget.querySelector('.footer_m_angle_down');
+  if (currentFooterLists.contains('display')) {
+    currentFooterLists.remove('display');
+    currentFooterIcons.classList.remove('open');
+  } else {
+    mobilefooterlists.forEach(e => e.classList.remove('display'));
+    mobilefooterIcons.forEach(e => e.classList.remove('open'));
+    currentFooterLists.add('display');
+    currentFooterIcons.classList.add('open');
+  }
+}
+
+mobilefooterlists.forEach(e => e.addEventListener('click', openFooterList));
 
